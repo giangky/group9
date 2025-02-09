f6d4083ec8453292a694702e6cd23cd019aefbf7
ogiblert1 <ogilber1@asu.edu>
2025-02-09 10:25:21 -0700

    added comment display from database

---
+++ ProjectTemplate/Post.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Post
+    {
+        //this is just a container for all info related
+        //to an post.  We'll simply create public class-level
+        //variables representing each piece of information!
+        public string title { get; set; }
+        public string content { get; set; }
+
+        
+    }
+}
--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -6,6 +6,10 @@ using System.Web.Services;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Security.Principal;
+using System.Web.UI.WebControls;
+using System.Runtime.Remoting.Messaging;
+using System.Configuration;
 
 namespace ProjectTemplate
 {
@@ -23,12 +27,12 @@ namespace ProjectTemplate
 		private string dbPass = "cis440springA2025team9";
 		private string dbName = "cis440springA2025team9";
 		////////////////////////////////////////////////////////////////////////
-		
+
 		////////////////////////////////////////////////////////////////////////
 		///call this method anywhere that you need the connection string!
 		////////////////////////////////////////////////////////////////////////
 		private string getConString() {
-			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName+"; UID=" + dbID + "; PASSWORD=" + dbPass;
+			return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName + "; UID=" + dbID + "; PASSWORD=" + dbPass;
 		}
 		////////////////////////////////////////////////////////////////////////
 
@@ -59,36 +63,70 @@ namespace ProjectTemplate
 			}
 			catch (Exception e)
 			{
-				return "Something went wrong, please check your credentials and db name and try again.  Error: "+e.Message;
+				return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
 			}
 		}
 
-        //Insert query for creating a post
-        [WebMethod(EnableSession = true)]
-        public void CreatePost(string title, string content)
-        {
+		//Insert query for creating a post
+		[WebMethod(EnableSession = true)]
+		public void CreatePost(string title, string content)
+		{
 
 			string sqlInsert = "INSERT into posts (title, content, created_at) " +
 				"values(@title, @content, NOW());";
 
+			MySqlConnection sqlConnection = new MySqlConnection(getConString());
+			MySqlCommand sqlCommand = new MySqlCommand(sqlInsert, sqlConnection);
+
+			sqlCommand.Parameters.AddWithValue("@title", HttpUtility.UrlDecode(title));
+			sqlCommand.Parameters.AddWithValue("@content", HttpUtility.UrlDecode(content));
+
+			sqlConnection.Open();
+
+			//we're using a try/catch so that if the query errors out we can handle it 
+			try
+			{
+				sqlCommand.ExecuteNonQuery();
+			}
+			catch (Exception e)
+			{
+			}
+			sqlConnection.Close();
+		}
+
+		[WebMethod(enableSession: true)]
+		public Post[] GetPosts()
+		{
+			
+			DataTable sqlDt = new DataTable("posts");
+
+			//Get connection string 
             MySqlConnection sqlConnection = new MySqlConnection(getConString());
-            MySqlCommand sqlCommand = new MySqlCommand(sqlInsert, sqlConnection);
 
-            sqlCommand.Parameters.AddWithValue("@title", HttpUtility.UrlDecode(title));
-            sqlCommand.Parameters.AddWithValue("@content", HttpUtility.UrlDecode(content));
+			//SQL query to select variables from table
+            string sqlSelect = "SELECT title, content FROM posts ORDER BY created_at DESC LIMIT 5";
 
-            sqlConnection.Open();
+			MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
 
-            //we're using a try/catch so that if the query errors out we can handle it 
-            try
-            {
-                sqlCommand.ExecuteNonQuery();
-            }
-            catch (Exception e)
-            {
-            }
-            sqlConnection.Close();
-        }
+			//Data adapter to fill data table
+			MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+			sqlDa.Fill(sqlDt);
 
-    }
+			//Lost to hold posts
+            List<Post> posts = new List<Post>();
+
+			for (int i = 0; i < sqlDt.Rows.Count; i++)
+			{
+				posts.Add(new Post
+				{
+					title = sqlDt.Rows[i]["title"].ToString(),
+					content = sqlDt.Rows[i]["content"].ToString()
+				});
+			}
+			//Return array 
+			return posts.ToArray();
+			
+
+		}
+	}
 }
