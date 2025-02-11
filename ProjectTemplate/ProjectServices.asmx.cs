6892894de8d79c0f2a608e5cc93845c8713a7f08
Ramon Acosta <racost17@asu.edu>
2025-02-10 22:59:48 -0700

    Merge pull request #2 fom giang

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -49,7 +49,7 @@ namespace ProjectTemplate
         {
             try
             {
-                string testQuery = "select * from test";
+                string testQuery = "select * from accounts";
 
                 ////////////////////////////////////////////////////////////////////////
                 ///here's an example of using the getConString method!
